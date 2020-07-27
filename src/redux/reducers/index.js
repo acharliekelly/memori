2a0aa643195c4a96209f31ac082a9cceb862d233
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-27 17:43:51 -0400

    fix reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,14 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import * as ACTIONS from '../actions/actionTypes';
 import { getDeck } from '../../api/deckApi';
 import { 
+  isMatched,
   initTiles,
   DEFAULT_DECK,
   DEFAULT_GRID
 } from '../../api/boardApi';
-import { isMatched } from '../../api/gameApi';
 
-const INITIAL_STATE = {
+export const INITIAL_STATE = {
   deck: { id: DEFAULT_DECK },
   gridSize: DEFAULT_GRID,
   moves: 0,
@@ -18,6 +18,7 @@ const INITIAL_STATE = {
   gameOver: false
 };
 
+
 /**
  * reducer
  * @param {Deck} state 
@@ -26,8 +27,11 @@ const INITIAL_STATE = {
  */
 const deckReducer = (state = INITIAL_STATE.deck, action) => {
   switch (action.type) {
-    case ACTIONS.CHANGE_DECK:
+    case ACTIONS.RESTART_GAME:
       return getDeck(state.id);
+    case ACTIONS.START_GAME:
+    case ACTIONS.CHANGE_DECK:
+      return getDeck(action.deckId);
     default:
       return state;
   }
@@ -45,6 +49,16 @@ const tileReducer = (state = [], action) => {
     case ACTIONS.START_GAME:
     case ACTIONS.RESIZE_GRID:
       return initTiles(action.gridSize);
+
+    case ACTIONS.CHANGE_DECK:
+      // flip all cards down
+      return state.map(tile => {
+        return {
+          ...tile,
+          flipped: false,
+          matched: false
+        }
+      });
 
     case ACTIONS.FLIP_CARD:
       return state.map((tile, index) => {
@@ -89,13 +103,14 @@ const tileReducer = (state = [], action) => {
   }
 }
 
+
 /**
  * reducer
  * @param {GameState} state 
  * @param {GameAction} action 
  * @returns {GameState}
  */
-const gameReducer = (state = INITIAL_STATE, action) => {
+const rootReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ACTIONS.START_GAME:
       return {
@@ -111,7 +126,8 @@ const gameReducer = (state = INITIAL_STATE, action) => {
         tiles: tileReducer([], { 
           type: ACTIONS.START_GAME,
           gridSize: state.gridSize
-        })
+        }),
+        deck: deckReducer(state.deck, action)
       };
 
     case ACTIONS.RESIZE_GRID:
@@ -127,7 +143,8 @@ const gameReducer = (state = INITIAL_STATE, action) => {
     case ACTIONS.CHANGE_DECK:
       return {
         ...state,
-        deck: deckReducer(state.deck, action)
+        deck: deckReducer(state.deck, action),
+        tiles: tileReducer(state.tiles, action)
       };
 
     case ACTIONS.FLIP_CARD:
@@ -162,72 +179,5 @@ const gameReducer = (state = INITIAL_STATE, action) => {
       return state;
   }
 }
-
-const rootReducer = combineReducers({
-  deckReducer,
-  tileReducer,
-  gameReducer
-});
-
-// /**
-//  * reducer
-//  * @param {GameState} state 
-//  * @param {GameAction} action 
-//  * @returns {GameState}
-//  */
-// const reducer = (state = INITIAL_STATE, action) => {
-//   const { secondFlip, deck, gridSize } = state;
-//   switch (action.type) {
-
-//     case ACTIONS.START_GAME:
-//       // use action params
-//       return {
-//         ...INITIAL_STATE,
-//         tiles: initTiles(action.gridSize),
-//         deck: getDeck(action.deckId)
-//       };
-
-//     case ACTIONS.CHANGE_DECK:
-//       return { ...state, deck: getDeck(action.deckId) }
-
-//     case ACTIONS.RESIZE_GRID:
-//       // resets game
-//       return {
-//         ...state,
-//         gridSize: action.gridSize,
-//         tiles: initTiles(action.gridSize),
-//         moves: 0,
-//         matches: 0,
-//         secondFlip: false
-//       };
-
-//     case ACTIONS.RESTART_GAME:
-//       // use state params
-//       return initGame(deck.id, gridSize);
-
-//     case ACTIONS.FLIP_CARD:
-//       const items = flip(state, action.index);
-//       return {
-//         ...state,
-//         tiles: items,
-//         secondFlip: !secondFlip
-//       };
-//     case ACTIONS.CHECK_MATCH:
-//       return afterSecondFlip(state);
-
-//     case ACTIONS.SHOW_WIN:
-//       return checkWin(state);
-
-//     case ACTIONS.SHOW_FACES:
-//       return {
-//         ...state,
-//         tiles: showAllFaces(state.tiles),
-//         secondFlip: false
-//       };
-      
-//     default:
-//       return state;
-//   }
-// };
 
 export default rootReducer;
