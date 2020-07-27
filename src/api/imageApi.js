2c4a35bf8e77d016d839bba5d042637e511d2307
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-27 05:34:16 -0400

    path setting

--- src/api/imageApi.js
+++ src/api/imageApi.js
@@ -4,6 +4,13 @@ const CLOUD_NAME = 'cantimaginewhy';
 
 const cloudUri = `https://res.cloudinary.com/${CLOUD_NAME}/`;
 
-export const getCloudImageUrl = (deckId, imageId, width = 60) => {
-  return `${cloudUri}/w_${width}/memory/decks/${deckId}/${imageId}`;
+/**
+ * return Cloudinary resource url
+ * @param {String} deckId deck id
+ * @param {String} imageId cloudinary file name (w/o dir)
+ * @param {String} path cloudinary directory
+ * @param {int} width image width (px)
+ */
+export const getCloudImageUrl = (deckId, imageId, path, width = 60) => {
+  return `${cloudUri}/w_${width}/${path}/${imageId}`;
 }
