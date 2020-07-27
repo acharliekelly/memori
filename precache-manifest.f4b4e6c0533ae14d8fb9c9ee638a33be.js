fc9ebbc8b875853b6ece263759bb0364daa4758f
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-27 17:47:32 -0400

    deploy task

---
+++ precache-manifest.f4b4e6c0533ae14d8fb9c9ee638a33be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413f0e0349b8222e44f368a846db3b6d",
+    "url": "/memori/index.html"
+  },
+  {
+    "revision": "6f0b5fd375419f7d5849",
+    "url": "/memori/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "57e172af370541e9a17d",
+    "url": "/memori/static/css/main.a67a49ed.chunk.css"
+  },
+  {
+    "revision": "6f0b5fd375419f7d5849",
+    "url": "/memori/static/js/2.1bac8a18.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/memori/static/js/2.1bac8a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e172af370541e9a17d",
+    "url": "/memori/static/js/main.2ce1e208.chunk.js"
+  },
+  {
+    "revision": "45baccbc33c5d3c57a3c",
+    "url": "/memori/static/js/runtime-main.2a66e34a.js"
+  }
+]);
