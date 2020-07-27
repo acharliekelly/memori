3e12732cf2e9eefdc519d92c25447aa8b955bf40
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-27 05:34:40 -0400

    timing

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -8,17 +8,16 @@ import {
 export default ({ index, tile, flipCard, deck }) => (
   <ReactCardFlip
     isFlipped={tile.flipped}
-    flipSpeedBackToFront={1.5}
-    flipSpeedFrontToBack={0.5} 
+    flipSpeedBackToFront={0.6}
+    flipSpeedFrontToBack={0.6}
+    flipDirection="horizontal" 
   >
-    <div 
-      key="front" 
+    <div  
       className="card card-back" 
       style={getCardBackStyle(deck)} 
       onClick={() => flipCard(index)} 
     />
-    <div 
-      key="back" 
+    <div  
       className="card card-face" 
       style={getCardFaceStyle(deck, tile)}>
         {deck.showText && (
